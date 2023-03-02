96cdd7a6243c1c875b3a4d60912d954ec4840a48
David Czarnecki <david.czarnecki@laposte.net>
2023-03-02 14:57:23 +0100

    added 'grille' as an argument, all the if/else lines may be not necessary, keeping the last command is maybe suffisant

--- detectMine.R
+++ detectMine.R
@@ -1,5 +1,5 @@
 #fonction qui donne le nombre de mines autours d'une case de coordonnées (i,j)
-detectMine <- function(i,j) {
+detectMine <- function(grille, i, j) {
   if (i==1 && j==1){n <- sum(grille[1:2,1:2])}
   else if (i==1 && j==8){n <- sum(grille[1:2,7:8])}
   else if (i==8 && j==8){n <- sum(grille[7:8,7:8])}
