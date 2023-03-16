8721ea45109047842ccfdad4b6acd5923365f50d
otmaneelllaki <otmane.allaki1@gmail.com>
2023-03-16 21:01:16 +0100

    add columns parametre to the board function

--- App/initialize_board.R
+++ App/initialize_board.R
@@ -1,17 +1,17 @@
-initialize_board <- function(rowNumber, MineNumber) {
+initialize_board <- function(rowNumber, colNumber, MineNumber) {
   
   # Create a blank rowNumber x rowNumber game board
-  board <- matrix(0, nrow = rowNumber, ncol = rowNumber)
+  board <- matrix(0, nrow = rowNumber, ncol = colNumber)
   
   # Randomly place MineNumber mines on the game board
-  mine_indices <- sample(1:rowNumber**2, MineNumber, replace = FALSE)
+  mine_indices <- sample(1:(rowNumber*colNumber), MineNumber, replace = FALSE)
   board[mine_indices] <- -1
   
   # Assign the appropriate number of mines to each button based on its adjacent buttons
   for (i in 1:rowNumber) {
-    for (j in 1:rowNumber) {
+    for (j in 1:colNumber) {
       if (board[i, j] != -1) {
-        num_adjacent_mines <- sum(board[max(i-1,1):min(i+1,rowNumber), max(j-1,1):min(j+1,rowNumber)] == -1)
+        num_adjacent_mines <- sum(board[max(i-1,1):min(i+1,rowNumber), max(j-1,1):min(j+1,colNumber)] == -1)
         board[i, j] <- num_adjacent_mines
       }
     }
