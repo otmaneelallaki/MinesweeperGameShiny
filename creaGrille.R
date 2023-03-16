38eff8ffde4d7a884c9544a6336e94f14d803926
David Czarnecki <david.czarnecki@laposte.net>
2023-03-16 09:02:08 +0100

    j'ai simplifié le code, pas de grand changement

--- creaGrille.R
+++ creaGrille.R
@@ -2,8 +2,9 @@
 #elle crée une ligne avec 10 mines (codées par des 1) et 53 cases vides (codées
 #par des 0) ensuite on ajoute un 0 à la position du premier clic du joueur
 
+
 creaGrille <- function(x,y){
-  ligne <- sample(c( rep(c(0,0,0,1,0,0), each = 10), rep(0,3) ))
+  ligne <- sample(c( rep(0,53) , rep(1,10) ))
   ligne <- c(ligne[1:(x*8+y)], 0, ligne[(x*8+y+1):63])
   grille <- matrix(data = ligne, nrow = 8, ncol = 8, byrow=TRUE)
 }
@@ -13,5 +14,3 @@ creaGrille <- function(x,y){
 a <- 4
 b <- 5
 grilleExemple <- creaGrille(a, b)
-
-
